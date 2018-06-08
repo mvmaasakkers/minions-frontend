91f05ee229513144af1b33ecf1f8739c6bb44320
Patrick Evers <pmdevers@gmail.com>
2018-06-08 22:08:46 +0200

    add  routing

--- LiveAsAService.UI/src/app/app.module.ts
+++ LiveAsAService.UI/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './services/login.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     LoginComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [
     LoginService
