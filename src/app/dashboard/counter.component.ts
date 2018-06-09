0773d77fbd6b02349c35bd3b78796082525acc6d
Wim <you@example.com>
2018-06-09 16:35:49 +0200

    remove progres counter

--- src/app/dashboard/counter.component.ts
+++ src/app/dashboard/counter.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ProgressCounterService } from '../services/progress-counter.service';
 
 @Component({
     selector: 'counter',
@@ -10,16 +9,9 @@ export class CounterComponent implements OnInit {
 
     @Input() value: number;
 
-    constructor(private progressCounterService: ProgressCounterService) {
-        console.log("ht");
-    }
+    constructor() { }
 
     ngOnInit() { 
-        //this.value.toString()
-        var a = this.progressCounterService.progress().subscribe(data => { 
-            console.log('v = ', data);
-        });
-        console.log('here', a);
         this.value.toString()
     }
 
