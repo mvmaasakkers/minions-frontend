c5fc8d70397d898811f4146ceed72a6aae4e6d05
Wim <you@example.com>
2018-06-10 07:15:45 +0200

    work

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BankService } from '../services/bank.service';
+
+
+const accId: string = "NLABNA0815453539";
 
 @Component({
     selector: 'transactions',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsComponent implements OnInit {
 
-    constructor() { }
+    constructor(
+        private bankService: BankService
+    ) { 
+        console.log("ctor");
+    }
 
     ngOnInit() {
-
+        console.log("init");
+        //this.bankService.accounts().subscribe(
+        //    r => console.log(r)
+        //)
+        this.bankService.transactions(accId).subscribe(
+            r => console.log(r)
+        )
     }
 
 }
