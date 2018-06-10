7210111e861477839e165977b003e4c9515c3bc5
Wim <you@example.com>
2018-06-10 09:20:51 +0200

    work

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BankService } from '../services/bank.service';
 
 
+const accId: string = "NLABNA0960275106";
 
 @Component({
     selector: 'transactions',
@@ -11,11 +12,17 @@ export class TransactionsComponent implements OnInit {
 
     constructor(
         private bankService: BankService
-    ) { 
+    ) {
         console.log("ctor");
     }
 
     ngOnInit() {
+        this.bankService.transactions(accId).subscribe(
+            r => {
+                console.log(r);
+                this.currentBalance = r.slice(-1)[0].details.new_balance.amount;
+                console.log("cb = ", this.currentBalance);
+            })
     }
 
 }
